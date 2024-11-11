6af7c564b0c70b0d97194080459e519d6803b470
suga <agus.sarwono@gmail.com>
2024-11-11 19:16:50 +0700

    update package

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import vercel from "vite-plugin-vercel";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercel()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
